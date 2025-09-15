3ac0d25cd9bd91dca4c2ff1afab07cfc7b961fb2
Rex Yen <kirtio0504@gmail.com>
2025-09-15 22:43:07 +0800

    修復部署問題

--- src/components/MapManagement.tsx
+++ src/components/MapManagement.tsx
@@ -102,7 +102,7 @@ if (typeof document !== 'undefined') {
 }
 
 // Fix for default markers in Leaflet
-delete (L.Icon.Default.prototype as L.Icon.Default)._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
